2244ba9a880a967dd8347ef75c3f0f650fd4da11
tkmpypy <tkmpypy@gmail.com>
2019-09-15 19:21:53 +0900

    update

--- .emacs.d/inits/09-dart.el
+++ .emacs.d/inits/09-dart.el
@@ -1,8 +1,27 @@
 (use-package dart-mode
   :config
-  (setq dart-enable-analysis-server nil))
+  (setq dart-enable-analysis-server nil)
+  :hook
+  (dart-mode-hook . tkmpypy/flutter-hot-reload-enable))
 
 (use-package flutter
   :after dart-mode
   :custom
   (flutter-sdk-path "~/flutter-sdk/flutter/"))
+
+(defun tkmpypy/flutter-hot-reload()
+	(interactive)
+	"Send a signal to daemon to hot reload."
+	(start-process "emacs-flutter-hot-reloader" nil "pkill" "-SIGUSR1" "-f" "flutter_tool"))
+(defun tkmpypy/flutter-hot-restart()
+	(interactive)
+	"Send a signal to daemon to hot restart."
+	(start-process "emacs-flutter-hot-reloader" nil "pkill" "-SIGUSR2" "-f" "flutter_tool"))
+(defun tkmpypy/flutter-hot-reload-enable()
+	(interactive)
+	"Enable flutter hot reload on save."
+	(add-hook 'after-save-hook 'tkmpypy/flutter-hot-reload t t))
+(defun tkmpypy/flutter-hot-reload-disable()
+	(interactive)
+	"Disable flutter hot reload on save."
+	(remove-hook 'after-save-hook 'tkmpypy/flutter-hot-reload t))
