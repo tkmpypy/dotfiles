a98bf5ebb0dbdece95749f75c49c60701b8366b9
tkmpypy <tkmpypy@gmail.com>
2019-09-17 13:40:49 +0900

    update

--- .emacs.d/inits/09-js2.el
+++ .emacs.d/inits/09-js2.el
@@ -1,8 +1,5 @@
-(use-package js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook
-    (lambda ()
-	(setq my-js-mode-indent-num 2)
-	(setq js2-basic-offset my-js-mode-indent-num)
-	(setq js-switch-indent-offset my-js-mode-indent-num)
-    ))
+(use-package js2-mode
+  :mode ("\\.js$" . js2-mode)
+  :init
+  (setq js2-basic-offset 2)
+  (setq js-switch-indent-offset 2))
--- .emacs.d/inits/09-web.el
+++ .emacs.d/inits/09-web.el
@@ -1,12 +1,12 @@
 (use-package web-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.hbs$" . web-mode))
-  ;; indent
-  (setq web-mode-html-offset   2)
-  (setq web-mode-style-padding 2)
-  (setq web-mode-css-offset    2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2)
-  (setq web-mode-auto-close-style 0))
+  :mode
+  ("\\.html$" . web-mode)
+  ("\\.hbs$" . web-mode)
+  :init
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq js-indent-level 2)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-expanding t)
+  (setq web-mode-enable-css-colorization t))
