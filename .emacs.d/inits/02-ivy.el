ecd2fd715784ccf288d7d12ebc97489f21ed0733
tkmpypy <tkm2126@gmail.com>
2019-09-28 19:00:34 +0900

    update

--- .emacs.d/inits/02-ivy.el
+++ .emacs.d/inits/02-ivy.el
@@ -41,3 +41,11 @@
   (ivy-posframe ((t (:background "#282a36"))))
     (ivy-posframe-border ((t (:background "#6272a4"))))
     (ivy-posframe-cursor ((t (:background "#61bfff")))))
+
+(use-package prescient)
+(use-package ivy-prescient
+  :after prescient
+  :config (ivy-prescient-mode))
+(use-package company-prescient
+  :after prescient
+  :config (company-prescient-mode))
