41d9d1dc360366ae09f7bc26ccdcf582ee6ce0f3
tkmpypy <tkmpypy@gmail.com>
2021-03-05 20:13:49 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -128,7 +128,6 @@ use {
 use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
 use {'lambdalisue/gina.vim'}
 use {'rhysd/git-messenger.vim'}
-use {'gotchane/vim-git-commit-prefix'}
 use {'APZelos/blamer.nvim'}
 
 -- service
