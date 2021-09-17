5af7f81584ce93eda9a086ba793369195a77f57a
tkmpypy <tkmpypy@gmail.com>
2021-09-17 19:13:49 +0900

    Update

--- .emacs.d/inits/09-python-mode.el
+++ .emacs.d/inits/09-python-mode.el
@@ -1,6 +1,1 @@
 (use-package python-mode)
-
-(use-package lsp-python-ms
-  :after python-mode
-  :custom
-  (lsp-python-ms-extra-paths '("/usr/local/bin" "/Users/takuma/unibo/nlp-server/nlpserver/src/main")))
