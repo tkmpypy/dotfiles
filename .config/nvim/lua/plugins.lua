4afdc4d0c15b48ffbfdd23ad7cfbc0f162571d09
tkmpypy <tkmpypy@gmail.com>
2022-03-07 19:11:34 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -753,7 +753,7 @@ packer.startup {
                 { key = "}c", cb = tree_cb "next_git_item" },
                 { key = "-", cb = tree_cb "dir_up" },
                 { key = "q", cb = tree_cb "close" },
-                { key = "g?", cb = tree_cb "toggle_help" },
+                { key = "?", cb = tree_cb "toggle_help" },
               },
             },
           },
