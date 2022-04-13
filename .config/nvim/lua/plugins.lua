a6a2067e204d6eaabc1f1a8358121f0afe8505ef
tkmpypy <tkmpypy@gmail.com>
2022-04-13 11:44:49 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1794,9 +1794,160 @@ packer.startup {
 
     -- Git
     use {
+      "tanvirtin/vgit.nvim",
+      requires = {
+        "nvim-lua/plenary.nvim",
+      },
+      config = function()
+        require("vgit").setup {
+          keymaps = {
+            ["n <C-k>"] = "hunk_up",
+            ["n <C-j>"] = "hunk_down",
+            ["n <leader>ghs"] = "buffer_hunk_stage",
+            ["n <leader>ghr"] = "buffer_hunk_reset",
+            ["n <leader>ghp"] = "buffer_hunk_preview",
+            ["n <leader>gbp"] = "buffer_blame_preview",
+            ["n <leader>gdp"] = "buffer_diff_preview",
+            ["n <leader>gbu"] = "buffer_reset",
+            ["n <leader>gbs"] = "buffer_stage",
+            ["n <leader>gbg"] = "buffer_gutter_blame_preview",
+            ["n <leader>gpl"] = "project_hunks_preview",
+            ["n <leader>gpd"] = "project_diff_preview",
+            ["n <leader>gpq"] = "project_hunks_qf",
+            ["n <leader>gx"] = "toggle_diff_preference",
+          },
+          settings = {
+            live_blame = {
+              enabled = true,
+              format = function(blame, git_config)
+                local config_author = git_config["user.name"]
+                local author = blame.author
+                if config_author == author then
+                  author = "You"
+                end
+                local time = os.difftime(os.time(), blame.author_time) / (60 * 60 * 24 * 30 * 12)
+                local time_divisions = {
+                  { 1, "years" },
+                  { 12, "months" },
+                  { 30, "days" },
+                  { 24, "hours" },
+                  { 60, "minutes" },
+                  { 60, "seconds" },
+                }
+                local counter = 1
+                local time_division = time_divisions[counter]
+                local time_boundary = time_division[1]
+                local time_postfix = time_division[2]
+                while time < 1 and counter ~= #time_divisions do
+                  time_division = time_divisions[counter]
+                  time_boundary = time_division[1]
+                  time_postfix = time_division[2]
+                  time = time * time_boundary
+                  counter = counter + 1
+                end
+                local commit_message = blame.commit_message
+                if not blame.committed then
+                  author = "You"
+                  commit_message = "Uncommitted changes"
+                  return string.format(" %s • %s", author, commit_message)
+                end
+                local max_commit_message_length = 255
+                if #commit_message > max_commit_message_length then
+                  commit_message = commit_message:sub(1, max_commit_message_length) .. "..."
+                end
+                return string.format(
+                  " %s, %s • %s",
+                  author,
+                  string.format(
+                    "%s %s ago",
+                    time >= 0 and math.floor(time + 0.5) or math.ceil(time - 0.5),
+                    time_postfix
+                  ),
+                  commit_message
+                )
+              end,
+            },
+            live_gutter = {
+              enabled = true,
+            },
+            authorship_code_lens = {
+              enabled = true,
+            },
+            screen = {
+              diff_preference = "unified",
+            },
+            project_diff_preview = {
+              keymaps = {
+                buffer_stage = "s",
+                buffer_unstage = "u",
+                stage_all = "a",
+                unstage_all = "d",
+                reset_all = "r",
+              },
+            },
+            signs = {
+              priority = 10,
+              definitions = {
+                GitSignsAddLn = {
+                  linehl = "GitSignsAddLn",
+                  texthl = nil,
+                  numhl = nil,
+                  icon = nil,
+                  text = "",
+                },
+                GitSignsDeleteLn = {
+                  linehl = "GitSignsDeleteLn",
+                  texthl = nil,
+                  numhl = nil,
+                  icon = nil,
+                  text = "",
+                },
+                GitSignsAdd = {
+                  texthl = "GitSignsAdd",
+                  numhl = nil,
+                  icon = nil,
+                  linehl = nil,
+                  text = "┃",
+                },
+                GitSignsDelete = {
+                  texthl = "GitSignsDelete",
+                  numhl = nil,
+                  icon = nil,
+                  linehl = nil,
+                  text = "┃",
+                },
+                GitSignsChange = {
+                  texthl = "GitSignsChange",
+                  numhl = nil,
+                  icon = nil,
+                  linehl = nil,
+                  text = "┃",
+                },
+              },
+              usage = {
+                screen = {
+                  add = "GitSignsAddLn",
+                  remove = "GitSignsDeleteLn",
+                },
+                main = {
+                  add = "GitSignsAdd",
+                  remove = "GitSignsDelete",
+                  change = "GitSignsChange",
+                },
+              },
+            },
+            symbols = {
+              void = "⣿",
+            },
+          },
+        }
+      end,
+    }
+    use {
       "lewis6991/gitsigns.nvim",
       requires = { "nvim-lua/plenary.nvim" },
       tag = "release",
+      disable = true,
       config = function()
         require("gitsigns").setup {
           signs = {
