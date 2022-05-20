2a1b5ed23b3dc2027fb7caa8232d1195d00b0988
tkmpypy <tkmpypy@gmail.com>
2022-05-20 10:30:04 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1698,7 +1698,7 @@ packer.startup {
                 input_prompt = "Grep For❯ ",
                 -- cmd               = "rg --vimgrep",
                 rg_opts = "--hidden --column --line-number --no-heading "
-                  .. "--color=always --with-filename --smart-case -g '!{.git,node_modules}/*'",
+                    .. "--color=always --with-filename --smart-case -g '!{.git,node_modules}/*'",
                 multiprocess = true,
                 git_icons = true, -- show git icons?
                 file_icons = true, -- show file icons?
@@ -2204,19 +2204,17 @@ packer.startup {
             fold_open = "",
           },
           file_panel = {
-            position = "left", -- One of 'left', 'right', 'top', 'bottom'
-            width = 35, -- Only applies when position is 'left' or 'right'
-            height = 10, -- Only applies when position is 'top' or 'bottom'
             listing_style = "tree", -- One of 'list' or 'tree'
             tree_options = { -- Only applies when listing_style is 'tree'
-              flatten_dirs = true,
-              folder_statuses = "always", -- One of 'never', 'only_folded' or 'always'.
+              flatten_dirs = true, -- Flatten dirs that only contain one single dir
+              folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
+            },
+            win_config = { -- See ':h diffview-config-win_config'
+              position = "left",
+              width = 35,
             },
           },
           file_history_panel = {
-            position = "bottom",
-            width = 35,
-            height = 16,
             log_options = {
               max_count = 256, -- Limit the number of commits
               follow = false, -- Follow renames (only for single file)
@@ -2225,22 +2223,35 @@ packer.startup {
               no_merges = false, -- List no merge commits
               reverse = false, -- List commits in reverse order
             },
+            win_config = { -- See ':h diffview-config-win_config'
+              position = "bottom",
+              height = 16,
+            },
           },
+          commit_log_panel = {
+            win_config = {}, -- See ':h diffview-config-win_config'
+          },
+          default_args = { -- Default args prepended to the arg-list for the listed commands
+            DiffviewOpen = {},
+            DiffviewFileHistory = {},
+          },
+          hooks = {}, -- See ':h diffview-config-hooks'
           key_bindings = {
             disable_defaults = false, -- Disable the default key bindings
             -- The `view` bindings are active in the diff buffers, only when the current
             -- tabpage is a Diffview.
             view = {
+              ["q"]     = cb("close"),
               ["<tab>"] = cb "select_next_entry", -- Open the diff for the next file
               ["<s-tab>"] = cb "select_prev_entry", -- Open the diff for the previous file
               ["gf"] = cb "goto_file", -- Open the file in a new split in previous tabpage
               ["<C-w><C-f>"] = cb "goto_file_split", -- Open the file in a new split
               ["<C-w>gf"] = cb "goto_file_tab", -- Open the file in a new tabpage
               ["<leader>e"] = cb "focus_files", -- Bring focus to the files panel
-              ["<leader>b"] = cb "toggle_files", -- Toggle the files panelos.time
-              ["q"] = "<CMD>DiffviewClose<CR>",
+              ["<leader>b"] = cb "toggle_files", -- Toggle the files panel.
             },
             file_panel = {
+              ["q"]     = cb("close"),
               ["j"] = cb "next_entry", -- Bring the cursor to the next file entry
               ["<down>"] = cb "next_entry",
               ["k"] = cb "prev_entry", -- Bring the cursor to the previous file entry.
@@ -2253,6 +2264,7 @@ packer.startup {
               ["U"] = cb "unstage_all", -- Unstage all entries.
               ["X"] = cb "restore_entry", -- Restore entry to the state on the left side.
               ["R"] = cb "refresh_files", -- Update stats and entries in the file list.
+              ["L"] = cb "open_commit_log", -- Open the commit log panel.
               ["<tab>"] = cb "select_next_entry",
               ["<s-tab>"] = cb "select_prev_entry",
               ["gf"] = cb "goto_file",
@@ -2262,11 +2274,13 @@ packer.startup {
               ["f"] = cb "toggle_flatten_dirs", -- Flatten empty subdirectories in tree listing style.
               ["<leader>e"] = cb "focus_files",
               ["<leader>b"] = cb "toggle_files",
-              ["q"] = "<CMD>DiffviewClose<CR>",
             },
             file_history_panel = {
+              ["q"]     = cb("close"),
               ["g!"] = cb "options", -- Open the option panel
-              ["<C-d>"] = cb "open_in_diffview", -- Open the entry under the cursor in a diffview
+              ["<C-A-d>"] = cb "open_in_diffview", -- Open the entry under the cursor in a diffview
+              ["y"] = cb "copy_hash", -- Copy the commit hash of the entry under the cursor
+              ["L"] = cb "open_commit_log",
               ["zR"] = cb "open_all_folds",
               ["zM"] = cb "close_all_folds",
               ["j"] = cb "next_entry",
@@ -2283,7 +2297,6 @@ packer.startup {
               ["<C-w>gf"] = cb "goto_file_tab",
               ["<leader>e"] = cb "focus_files",
               ["<leader>b"] = cb "toggle_files",
-              ["q"] = "<CMD>DiffviewClose<CR>",
             },
             option_panel = {
               ["<tab>"] = cb "select",
@@ -2531,17 +2544,17 @@ packer.startup {
       use {
         "neovim/nvim-lspconfig",
         requires = {
-          use {"williamboman/nvim-lsp-installer"},
+          use { "williamboman/nvim-lsp-installer" },
           use {
             "someone-stole-my-name/yaml-companion.nvim",
             requires = {
               "nvim-lua/plenary.nvim",
               "nvim-telescope/telescope.nvim",
             },
-            config = function ()
-              require("telescope").load_extension("yaml_schema")
-            end
-          }
+            config = function()
+              require("telescope").load_extension "yaml_schema"
+            end,
+          },
         },
         config = function()
           require("nvim-lsp-installer").setup {}
