3d9e77068b66d2a784bf352acbfdae1681219830
tkmpypy <tkmpypy@gmail.com>
2022-06-13 15:19:06 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -47,16 +47,18 @@ packer.startup {
       "rebelot/kanagawa.nvim",
       config = function()
         require("kanagawa").setup {
-          undercurl = true, -- enable undercurls
-          commentStyle = "italic",
-          functionStyle = "NONE",
-          keywordStyle = "italic",
-          statementStyle = "bold",
-          typeStyle = "NONE",
-          variablebuiltinStyle = "italic",
-          specialReturn = true, -- special highlight for the return keyword
-          specialException = true, -- special highlight for exception handling keywords
-          transparent = false, -- do not set background color
+          undercurl = true,           -- enable undercurls
+          commentStyle = { italic = true },
+          functionStyle = {},
+          keywordStyle = { italic = true},
+          statementStyle = { bold = true },
+          typeStyle = {},
+          variablebuiltinStyle = { italic = true},
+          specialReturn = true,       -- special highlight for the return keyword
+          specialException = true,    -- special highlight for exception handling keywords
+          transparent = false,        -- do not set background color
+          dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+          globalStatus = false,       -- adjust window separators highlight for laststatus=3
           colors = {},
           overrides = {},
         }
