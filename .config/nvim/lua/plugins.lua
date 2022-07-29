ae963c5634c51aaa4e892c6195fce15e06784841
tkmpypy <tkmpypy@gmail.com>
2022-07-29 19:08:12 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2712,7 +2712,7 @@ packer.startup {
         config = function()
           require("fidget").setup {
             text = {
-              spinner = "pipe", -- animation shown when tasks are ongoing
+              spinner = "meter", -- animation shown when tasks are ongoing
               done = "✔", -- character shown when all tasks are complete
               commenced = "Started", -- message shown when task starts
               completed = "Completed", -- message shown when task completes
