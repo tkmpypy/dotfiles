47b303beae5a7a995467d8210a853df260a06933
tkmpypy <tkmpypy@gmail.com>
2022-10-19 18:34:22 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -915,7 +915,7 @@ packer.startup {
       config = function()
         local alpha = require "alpha"
         local theme = require "alpha.themes.theta"
-        local dashboard = require("alpha.themes.dashboard")
+        local dashboard = require "alpha.themes.dashboard"
 
         -- Set header
         local header = {
@@ -970,20 +970,33 @@ packer.startup {
           cmdline = {
             view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
             opts = { buf_options = { filetype = "vim" } }, -- enable syntax highlighting in the cmdline
-            icons = {
-              ["/"] = { icon = " ", hl_group = "DiagnosticWarn" },
-              ["?"] = { icon = " ", hl_group = "DiagnosticWarn" },
-              [":"] = { icon = " ", hl_group = "DiagnosticInfo", firstc = false },
-            },
+          },
+          messages = {
+            -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+            -- This is a current Neovim limitation.
+            enabled = true, -- enables the Noice messages UI
+          },
+          popupmenu = {
+            enabled = true, -- enables the Noice popupmenu UI
+            ---@type 'nui'|'cmp'
+            backend = "nui", -- backend to use to show regular cmdline completions
           },
           history = {
             -- options for the message history that you get with `:Noice`
             view = "split",
             opts = { enter = true },
+            filter = { event = { "msg_show", "notify" }, ["not"] = { kind = { "search_count", "echo" } } },
           },
-          popupmenu = {
-            enabled = true, -- disable if you use something like cmp-cmdline
-            backend = "nui", -- 'nui' | 'cmp'
+          notify = {
+            -- Noice can be used as `vim.notify` so you can route any notification like other messages
+            -- Notification messages have their level and other properties set.
+            -- event is always "notify" and kind can be any log level as a string
+            -- The default routes will forward notifications to nvim-notify
+            -- Benefit of using Noice for this is the routing and consistent history view
+            enabled = true,
+          },
+          lsp_progress = {
+            enabled = false,
           },
         }
       end,
