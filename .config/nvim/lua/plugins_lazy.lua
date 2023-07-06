b67998efa98a56ec7716f866b4fa652e64cf43f5
tkmpypy <tkmpypy@gmail.com>
2023-07-06 09:21:31 +0900

    Update

--- .config/nvim/lua/plugins_lazy.lua
+++ .config/nvim/lua/plugins_lazy.lua
@@ -1806,11 +1806,17 @@ require("lazy").setup({
     "akinsho/toggleterm.nvim",
     config = function()
       require("toggleterm").setup({
-        size = 20,
+        size = function(term)
+          if term.direction == "horizontal" then
+            return 15
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
+        end,
         open_mapping = [[<c-t>]],
         shade_filetypes = {},
         shade_terminals = false,
-        direction = "float",
+        direction = "horizontal",
         insert_mappings = true,
         close_on_exit = false,
         persist_size = false,
