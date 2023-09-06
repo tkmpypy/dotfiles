ca0645d6424f038eec5224784ff3f06fbe9fc99f
tkmpypy <tkmpypy@gmail.com>
2023-09-06 14:28:09 +0900

    chore: Rename and update p10k.zsh and sync.zsh

--- .config/zsh/sync.zsh
+++ .config/zsh/sync.zsh
@@ -15,7 +15,7 @@ fi
 # fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ ! -f "${XDG_CONFIG_HOME}/zsh/p10k.zsh" ]] || source "${XDG_CONFIG_HOME}/zsh/p10k.zsh"
 
 eval "$(rtx activate zsh)"
 eval "$(direnv hook zsh)"
