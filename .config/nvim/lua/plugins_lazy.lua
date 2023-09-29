ee49f51b9c3ae59239a90c03bc121c20a96c0f60
tkmpypy <tkmpypy@gmail.com>
2023-09-29 10:06:49 +0900

    feat(plugins): update indent-blankline.nvim configuration
    - Register custom highlight colors for indent levels
    - Use `ibl` as the main branch for `indent-blankline.nvim`

--- .config/nvim/lua/plugins_lazy.lua
+++ .config/nvim/lua/plugins_lazy.lua
@@ -788,35 +788,31 @@ require("lazy").setup({
   },
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     event = { "VeryLazy" },
     config = function()
-      -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f blend=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a blend=nocombine]]
-      require("indent_blankline").setup({
-        -- char = "",
-        enabled = true,
-        buftype_exclude = { "terminal", "help", "nofile" },
-        filetype_exclude = { "startify", "alpha", "NvimTree", "neo-tree" },
-        show_end_of_line = false,
-        -- space_char_blankline = " ",
-        show_trailing_blankline_indent = false,
-        -- char_highlight_list = {
-        --     "IndentBlanklineIndent1",
-        --     "IndentBlanklineIndent2",
-        --     "IndentBlanklineIndent3",
-        --     "IndentBlanklineIndent4",
-        --     "IndentBlanklineIndent5",
-        --     "IndentBlanklineIndent6",
-        -- },
-        show_current_context = false,
-        show_current_context_start = false,
-      })
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
+      require("ibl").setup({ scope = { highlight = highlight } })
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
   },
   {
