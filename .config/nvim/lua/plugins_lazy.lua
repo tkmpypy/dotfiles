55d7793cc2dca79c49eede8333d6886121724d8c
tkmpypy <tkmpypy@gmail.com>
2023-10-02 09:05:04 +0900

    Update

--- .config/nvim/lua/plugins_lazy.lua
+++ .config/nvim/lua/plugins_lazy.lua
@@ -811,7 +811,12 @@ require("lazy").setup({
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
 
-      require("ibl").setup({ scope = { highlight = highlight } })
+      require("ibl").setup({
+        scope = { highlight = highlight },
+        indent = {
+          tab_char = "▎"
+        }
+      })
       hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
   },
