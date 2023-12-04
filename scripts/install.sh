07fd909af38cf707461027014e695ed7d8bef04e
tkmpypy <tkmpypy@gmail.com>
2023-12-04 10:12:54 +0900

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -20,6 +20,7 @@ function link_file() {
   if [ $dir_name = "." ]; then
     exec_cmd "ln -sf $1/$2 $3"
   else
+    mkdir -p $3/$dir_name
     exec_cmd "ln -sf $1/$2 $3/$dir_name"
   fi
 }
@@ -38,7 +39,7 @@ function link() {
 
   if [[ -n "$XDG_CONFIG_HOME" ]]; then
     exec_cmd "mkdir -p $XDG_CONFIG_HOME"
-    CONFIG_FILES=(git nvim wezterm k9s rtx helix aqua sheldon zsh)
+    CONFIG_FILES=(git nvim wezterm k9s/skin.yml rtx helix aqua sheldon zsh)
     for file in ${CONFIG_FILES[@]}; do
       link_file $DOTFILES_DIR/.config $file $XDG_CONFIG_HOME
     done
