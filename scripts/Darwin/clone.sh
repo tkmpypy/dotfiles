2cf11bf7ac524d9b6ca27999bb57594b832c8635
tkmpypy <tkmpypy@gmail.com>
2024-01-04 18:16:44 +0900

    fix: remove homebrew installation from clone script

--- scripts/Darwin/clone.sh
+++ scripts/Darwin/clone.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
 # install tpm
 if [[ ! -d $HOME/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
