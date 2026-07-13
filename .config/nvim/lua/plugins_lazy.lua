25ec246f3525019ee2cd292723c6c418ccc691ef
Takuma Kimura <takuma-kimura@bm-sms.co.jp>
2026-07-13 15:45:25 +0900

    Update

--- .config/nvim/lua/plugins_lazy.lua
+++ .config/nvim/lua/plugins_lazy.lua
@@ -144,41 +144,6 @@ require("lazy").setup({
         desc = "Git Branches",
       },
       {
-        "<leader>gl",
-        function()
-          Snacks.picker.git_log()
-        end,
-        desc = "Git Log",
-      },
-      {
-        "<leader>gL",
-        function()
-          Snacks.picker.git_log_line()
-        end,
-        desc = "Git Log Line",
-      },
-      {
-        "<leader>gs",
-        function()
-          Snacks.picker.git_status()
-        end,
-        desc = "Git Status",
-      },
-      {
-        "<leader>gS",
-        function()
-          Snacks.picker.git_stash()
-        end,
-        desc = "Git Stash",
-      },
-      {
-        "<leader>gd",
-        function()
-          Snacks.picker.git_diff()
-        end,
-        desc = "Git Diff (Hunks)",
-      },
-      {
         "<leader>gf",
         function()
           Snacks.picker.git_log_file()
